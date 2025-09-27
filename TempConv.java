5b08bfab31ccf192aab1e70881588e9671d69ca4
SopeFetz <fetu.adesope@gmail.com>
2025-09-27 14:53:26 +0100

    Update TempConv.java
    Adjusted the code to fit the syllabus requirements.

--- TempConv.java
+++ TempConv.java
@@ -1,14 +1,10 @@
 // This import gets input from user.
 import java.util.Scanner;
 
-// The following imports aids in rouding up the celsius value.
-import java.math.BigDecimal;
-import java.math.RoundingMode;
-
 public class TempConv {
     public static void main(String[] args) {
         
-        //Displays program name and prompts the user  for input.
+        // Displays program name and prompts the user for input.
         System.out.println("Temperaure Converter");
         Scanner tempInput = new Scanner(System.in);
         System.out.print("Enter Temperature In Fahrenheit: ");
@@ -18,10 +14,10 @@ public class TempConv {
         double tempCelsius = (5.0 / 9.0 )*(tempFahren - 32);
 
         // This rounds the celsius value to 2 decimal places.
-        BigDecimal roundedCelsuis = new BigDecimal(tempCelsius).setScale(2, RoundingMode.HALF_UP);
+        double roundCelsius = Math.round(tempCelsius * 100.0) / 100.0;
 
         // Prints the rounded celsius value.
-        System.out.println("Temperature in Celsius = " + roundedCelsuis);
+        System.out.println("Temperature in Celsius = " + roundCelsius );
 
         tempInput.close();
     }
